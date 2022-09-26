72815546a9ec2f1a18325a876a1bc12bee8a0ff8
Maximiliano <brotzumaxx@gmail.com>
2022-09-26 19:45:42 -0300

    Implementada pantalla de carga en ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,6 +5,7 @@ import ItemList from "./ItemList";
 import { Link, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../context/Context";
+import PantallaDeCarga from "./PantallaDeCarga";
 
 
 function ItemListContainer() {
@@ -13,15 +14,34 @@ function ItemListContainer() {
     const [itemsCategoria, setItemsCategoria] = useState([]);
     const { cartTotal } = useContext(CartContext);
 
-    function GoToCart(){
-        if(cartTotal() > 0){
-            return(
+    function GoToCart() {
+        if (cartTotal() > 0) {
+            return (
                 <Link to={"/canje-de-puntos/cart"}>
                     <span>{cartTotal()}</span>
                     <span>Mis pedidos</span>
                 </Link>
             )
         }
+    }
+
+    function CargarListaDeItems() {
+        return (
+            <div>
+                <div className="ItemListContainerTittle">
+                    <h2>Sistema de canje de puntos</h2>
+                    {GoToCart()}
+                </div>
+                <div className="Categorias">
+                    <Link to={`/canje-de-puntos`}>
+                        <button className="btnTodos" onClick={() => { setItemsCategoria(items) }}>Todos</button>
+                    </Link>
+                    <Link to={`/canje-de-puntos/categoria/Viajes`}><button className="btnViajes">Viajes</button></Link>
+                    <Link to={`/canje-de-puntos/categoria/Electronica`}><button className="btnElectronica">Electronica</button></Link>
+                </div>
+                <ItemList items={itemsCategoria} />
+            </div>
+        )
     }
 
     useEffect(() => {
@@ -47,18 +67,7 @@ function ItemListContainer() {
 
     return (
         <div className="ItemListContainer">
-            <div className="ItemListContainerTittle">
-                <h2>Sistema de canje de puntos</h2>
-                {GoToCart()}
-            </div>
-            <div className="Categorias">
-                <Link to={`/canje-de-puntos`}>
-                    <button className="btnTodos" onClick={() => { setItemsCategoria(items) }}>Todos</button>
-                </Link>
-                <Link to={`/canje-de-puntos/categoria/Viajes`}><button className="btnViajes">Viajes</button></Link>
-                <Link to={`/canje-de-puntos/categoria/Electronica`}><button className="btnElectronica">Electronica</button></Link>
-            </div>
-            <ItemList items={itemsCategoria} />
+            {items.length < 1 ? <PantallaDeCarga /> : CargarListaDeItems()}
         </div>
     )
 }
---
+++ src/components/PantallaDeCarga.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PantallaDeCarga(){
+    return(
+        <div className="PantallaDeCarga">
+            <img src="https://c.tenor.com/tEBoZu1ISJ8AAAAC/spinning-loading.gif" alt="Cargando..." />
+        </div>
+    )
+}
+
+export default PantallaDeCarga;
