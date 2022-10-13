43cda8075ad9767a75ef69e293844c9765a47389
Maximiliano <brotzumaxx@gmail.com>
2022-10-13 20:02:02 -0300

    Detalles añadidos a pagina de inicio

--- src/components/MensajeBienvenida.jsx
+++ src/components/MensajeBienvenida.jsx
@@ -6,6 +6,9 @@ function MensajeBienvenida() {
     const { CloseSession } = useContext(CartContext);
     const { user } = useContext(CartContext);
 
+    let date = new Date();
+    let fechaActual = date.getDate() + "/" + (date.getMonth() + 1);
+
     function CerrarSesion() {
         CloseSession();
     }
@@ -17,6 +20,26 @@ function MensajeBienvenida() {
             <p>
                 ¿No es usted?<button onClick={CerrarSesion}>Cerrar sesión</button>
             </p>
+            <hr />
+            <div className="Container">
+                <p>$38.980,56</p>
+                <div className="InfoCuentas">
+                    <span>Saldo total en tus cuentas al {fechaActual}</span>
+                    <button>Ver Cuentas</button>
+                </div>
+
+
+                <div className="MasDetalles">
+                    <div>
+                        <p>Tenés 1 Cuenta</p>
+                        <p>↴</p>
+                    </div>
+                    <div>
+                        <p>Últimos movimientos de tu Caja de Ahorro en pesos 221-9999999-1</p>
+                        <p>↴</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
